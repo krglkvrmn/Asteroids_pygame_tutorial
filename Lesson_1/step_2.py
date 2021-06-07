62a3e9415f1209e13b5e55df2d50574f6ba3fff2
Kruglikov Roman <kruglikov1911@mail.ru>
2021-06-07 20:13:13 +0300

    Typo fixes

--- Lesson_1/step_2.py
+++ Lesson_1/step_2.py
@@ -1,8 +1,8 @@
 # Шаг 2 - шаблон игры на pygame, реализованный при помощи классов
 
-import sys      
-import os          
-import pygame      
+import sys
+import os
+import pygame
 import numpy as np
 
 
@@ -14,16 +14,17 @@ SCREEN_SIZE = screen.get_size()
 
 class Game:
     """Основной класс игры. В нём представлены основные методы для управления игровыми объектами, а также сами объекты"""
-    def  __init__(self, screen):     
-        self.screen = screen         
+
+    def __init__(self, screen):
+        self.screen = screen
         self.starship = Starship()  # Объект класса Starship (игрок)
-        self.bullets = []           # Заготовка для хранения объектов пуль
-        self.asteroids = []         # Заготовка для хранения объектов астероидов
+        self.bullets = []  # Заготовка для хранения объектов пуль
+        self.asteroids = []  # Заготовка для хранения объектов астероидов
 
     def handle_events(self):
         """Метод - обработчик событий. Выполняет те же действия, что и в базовом шаблоне"""
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 sys.exit()
 
     def draw(self, objects_list):
@@ -40,8 +41,10 @@ class Game:
             self.handle_events()
             self.draw([[self.starship]])
 
+
 class Starship:
     """Класс представляющий игрока"""
+
     def __init__(self):
         # Текущие координаты (в пикселях)
         self.pos = np.array([SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 2])
@@ -50,8 +53,8 @@ class Starship:
         # Изображение для отрисовки
         self.image = self.original_image
         # Границы модельки корабля (хитбокс)    
-        self.rect = self.image.get_rect(center=self.pos)     
+        self.rect = self.image.get_rect(center=self.pos)
 
 
-game = Game(screen)     # Создание и запуск игры
+game = Game(screen)  # Создание и запуск игры
 game.run()
